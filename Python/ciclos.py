61e3ce84d484b9fa085dcf1219082b9686c74953
Pitty Ajun, David <david.pitty.ajun@intel.com>
2024-06-08 13:58:40 -0600

    más ejemplos ciclos

--- Python/ciclos.py
+++ Python/ciclos.py
@@ -1,4 +1,17 @@
-print("BREAK")
+print("WHILE")
+lista_de_numeros = [1, 2, 3, 4, 5]
+indice = 0
+while indice < len(lista_de_numeros):
+    numero = lista_de_numeros[indice]
+    print(numero)
+    indice += 1
+
+print("\nFOR")
+for numero in lista_de_numeros:
+    print(numero)
+
+
+print("\nBREAK")
 for contador in range(1, 10):
     if contador % 2 == 0:
         break
